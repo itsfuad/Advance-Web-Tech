b9f75ddc4fbe395359004dff46f7fba6e5bf4d79
Fuad Hasan <fuad.cs22@gmail.com>
2026-03-23 18:14:13 +0600

    update UI

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -149,7 +149,7 @@ export default function Navbar() {
                         setAccountOpen(false);
                         handleLogout();
                       }}
-                      className="flex w-full items-center gap-3 px-4 py-3 text-sm text-left transition-colors hover:bg-(--secondary-hover)"
+                      className="text-red-500 cursor-pointer flex w-full items-center gap-3 px-4 py-3 text-sm text-left transition-colors hover:bg-(--secondary-hover)"
                     >
                       <LogOut size={16} />
                       Logout
