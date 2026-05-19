0c8d90692a3baf94d574c4e195f6dacbfa1338df
Fuad Hasan <fuad.cs22@gmail.com>
2026-05-19 20:42:58 +0600

    sanitize and add http endpoint test

---
+++ backend/src/common/sanitize.util.ts
@@ -0,0 +1,12 @@
+export function escapeHtml(input: string): string {
+  return input
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
+export function sanitizePlainText(input: string): string {
+  return escapeHtml(input.trim());
+}
